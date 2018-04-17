a5c86c9e3cca0e17659937871a181faf8702a3a7
Withee Poositasai <witheep@gmail.com>
2018-04-17 16:13:00 +0700

    Add axios, fetch basic data from opendota api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    heroes: []
+  };
+
+  componentDidMount() {
+    axios.get("https://api.opendota.com/api/heroStats").then(res => {
+      this.setState({ heroes: res.data });
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ol>
+          { this.state.heroes.map( hero => <li key={hero.id}>{hero.localized_name}</li>)}
+        </ol>
       </div>
     );
   }
