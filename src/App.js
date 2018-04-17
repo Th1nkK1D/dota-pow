1fa01abe42dacc3e05d0591fa69c3c1b48242f10
Withee Poositasai <witheep@gmail.com>
2018-04-17 23:38:07 +0700

    Basic suggestion implemented

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
     this.state = {
       heroes: [],
       foes: [undefined,undefined,undefined,undefined,undefined],
-      matchup: [undefined,undefined,undefined,undefined,undefined]
+      matchups: [undefined,undefined,undefined,undefined,undefined],
+      suggestions: []
     };
 
     this.handleFoeChange = this.handleFoeChange.bind(this)
@@ -20,7 +21,7 @@ class App extends Component {
   componentDidMount() {
     // Get heroes list
     axios.get("https://api.opendota.com/api/heroStats").then(res => {
-      this.setState({ heroes: res.data })
+      this.setState({ heroes: res.data.sort((a,b) => a.localized_name.localeCompare(b.localized_name)) })
     })
   }
 
@@ -30,16 +31,41 @@ class App extends Component {
 
     // Get matchup data
     axios.get("https://api.opendota.com/api/heroes/"+target.value+"/matchups").then(res => {
+      // Filter, calculate rating and sort match up
+      let matchup = res.data.filter(m => m.games_played >= 5)
+        .map(m => {m.rating = 1-m.wins/m.games_played; return m})
+        .sort((a,b) => b.rating-a.rating)
+
+      let suggestions = this.state.heroes.map(h => ({id: h.id, sum: 0}))
+
       // Update state
       this.setState((prev) => {
         prev.foes[target.id] = parseInt(target.value,10)
-        prev.matchup[target.id] = res.data
+        prev.matchups[target.id] = matchup
 
-        console.log(prev)
+        // Calculate suggestion
+        let count = 0
+
+        for (let h = 0; h < this.state.matchups.length; h++) {
+          if (this.state.matchups[h] !== undefined) {
+            for (let m = 0; m < this.state.matchups[h].length; m++) {
+              let target = suggestions.findIndex(s => s.id === this.state.matchups[h][m].hero_id)
+
+              if (target >= 0) {
+                suggestions[target].sum += this.state.matchups[h][m].rating
+              } else {
+                suggestions[target].sum += 0.5
+              }
+            }
+
+            count++
+          }
+        }
 
         return {
           foes: prev.foes,
-          matchup: prev.matchup
+          matchups: prev.matchups,
+          suggestions: suggestions.map(s => ({id: s.id, avg: s.sum/count})).sort((a,b) => b.avg-a.avg).slice(0,5)
         }
       })
     })
@@ -48,18 +74,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ol>
-        {
-          this.state.foes.map((hero,index) => 
-            <li key={index}>{hero}</li>
-          )
-        }
-        </ol>
+      <h3>Opponent Heroes:</h3>
         {
           this.state.foes.map((hero, index) => 
             <HeroSelecter heroes={this.state.heroes} key={index} id={index} value={hero} onChange={this.handleFoeChange}></HeroSelecter>
           )
         }
+        <h3>Suggestions:</h3>
+        <ol>
+        {
+          this.state.suggestions.map((hero,hi) => 
+            <li key={hero.id}>
+              {this.state.heroes.find(h => h.id === hero.id).localized_name} | 
+              {this.state.matchups.map((matchup, mi) => 
+                <span key={mi}> {
+                  matchup !== undefined 
+                    ? (matchup.findIndex(m => m.hero_id === hero.id) >= 0
+                      ? matchup.find(m => m.hero_id === hero.id).rating
+                      : "?"
+                    ) 
+                    : " "
+                } | </span>
+              )}
+              overall: {hero.avg}
+            </li>
+          )
+        }
+        </ol>
       </div>
     );
   }
--- src/HeroSelecter/HeroSelecter.js
+++ src/HeroSelecter/HeroSelecter.js
@@ -9,8 +9,8 @@ class HeroSelecter extends Component {
   render() {
     return (
       <div className="hero-selecter">
-        {this.props.value && this.props.heroes.find(h => h.id === this.props.value).localized_name}
-        <br/>
+        {/* {this.props.value && this.props.heroes.find(h => h.id === this.props.value).localized_name}
+        <br/> */}
         <select name="herolist" id={this.props.id} onChange={this.props.onChange}>
           <option value={undefined}>-</option>
           {this.props.heroes.map(hero => <option key={hero.id} value={hero.id}>{hero.localized_name}</option>)}
